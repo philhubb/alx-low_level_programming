c730d822247d5637990ce7df456baf8345aa87d5
Philip Onyenobi <feeliponyenobi@gmail.com>
2022-04-08 14:27:15 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,11 @@
-# ! / bin / bash
-gcc -c puts("Programming is like building a multilingual puzzle")
-gcc - c puts(0)
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
