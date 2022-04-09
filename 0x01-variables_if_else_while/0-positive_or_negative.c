39d600aff91c3c21d9b93f62ac1fdd2473dc86cb
Philip Onyenobi <feeliponyenobi@gmail.com>
2022-04-08 18:48:27 -0700

    If else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes here */
-
-/* betty style doc always success */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
