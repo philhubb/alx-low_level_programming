9e653aeca0827e3027680ed31e6a8fd97ed920b7
Philip Onyenobi <feeliponyenobi@gmail.com>
2022-04-09 07:58:52 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,7 +10,7 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 
 	return (1);
 }
