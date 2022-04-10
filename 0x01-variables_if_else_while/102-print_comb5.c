6692ce162e335e5dcbe5abb367ebe4c351c2f056
Philip Onyenobi <feeliponyenobi@gmail.com>
2022-04-10 02:00:48 -0700

    102 print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int y, z;
+
+	for (y = 0; y <= 98; y++)
+	{
+		for (z = y + 1; z <= 99; z++)
+		{
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			putchar(' ');
+			putchar((z / 10) + '0');
+			putchar((z % 10) + '0');
+
+			if (x == 98 && z == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
