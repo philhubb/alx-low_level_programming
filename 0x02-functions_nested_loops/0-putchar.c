e6fa58f4b47015413022f12b1e77140dc4c3b6dc
Philip Onyenobi <feeliponyenobi@gmail.com>
2022-04-12 01:31:03 -0700

    Putchar alphabets

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		_putchar(low);
+	}
+	_putchar('\n');
+	return(0);
+}
