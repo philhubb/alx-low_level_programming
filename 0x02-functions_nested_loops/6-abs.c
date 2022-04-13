08b87027937914f583bf38a8747a2e02c9469317
Philip Onyenobi <feeliponyenobi@gmail.com>
2022-04-12 17:24:26 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,31 +1,16 @@
 #include "main.h"
 
 /**
- * print_sign - Check Main
- * @n: An input number
- * Description: Tohis function prints the sign of the number
- * Return: 1 if number is positive, o if number is 0, or
- * -1 if number is negative
+ *_abs - Check Main
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
  */
-int print_sign(int n)
+
+int _abs(int r)
 {
-	int value;
-
-	if (n > 0)
-	{
-		value = 1;
-		_putchar('+');
-	}
-	else if (n == 0)
-	{
-		value = 0;
-		_putchar('0');
-	}
+	if (r >= 0)
+		return (r);
 	else
-	{
-		value = -1;
-		_putchar('-');
-	}
-
-	return (value);
+		return (r * -1);
 }
