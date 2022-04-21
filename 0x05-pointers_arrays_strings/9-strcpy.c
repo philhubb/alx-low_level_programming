c6b8f2bbd70437a3893a12f1b3cf3a0fb06a4bdc
Philip Onyenobi <feeliponyenobi@gmail.com>
2022-04-20 21:57:37 -0700

    9-strcpy.c1

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *copy = dest;
+	int i = 0;
 
-	while (*src)
-		*dest++ = *src++;
-	return (copy);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
