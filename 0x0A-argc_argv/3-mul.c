c9d78c12f7e268001d70f0d94062440af2bb74ec
philhubb <feeliponyenobi@gmail.com>
2022-05-03 01:02:13 +0100

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints all arguments it receives
+ * main - multiplies two numbers
  * @argc: n args
  * @argv: arr args
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, val = 1;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		val *= atoi(argv[i]);
+	}
+	printf("%d\n", val);
 	return (0);
 }
