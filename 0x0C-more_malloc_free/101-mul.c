8aee0a97021472557571abab5f58cda46c31802b
philhubb <feeliponyenobi@gmail.com>
2022-05-10 15:18:20 +0100

    more malloc free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,19 +9,26 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
+	int len_1, len_2, len_r, temp, *sum_result;
+	char *num1, *num2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	len_1 = str_len(argv[1]), len_2 = str_len(argv[2]);
+	len_r = len_1 + len_2;
+	if (len_1 < len_2)
+		num1 = argv[1], num2 = argv[2];
 	else
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
+		num1 = argv[2], num2 = argv[1];
+		temp = len_2, len_2 = len_1, len_1 = temp;
 	}
+	sum_result = multiply(num1, len_1, num2, len_2, len_r);
+	if (sum_result == NULL)
+		exit(98);
+	print_me(sum_result, len_r);
+	return (0);
 }
