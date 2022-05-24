9379809c4ed840cca55c3b77df4c3364318d5623
philhubb <feeliponyenobi@gmail.com>
2022-05-25 00:53:55 +0100

    Tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
