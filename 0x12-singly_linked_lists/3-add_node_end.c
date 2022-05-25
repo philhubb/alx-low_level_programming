3bc181e6ebc90c9cde5491b3aa8a21a4a7e4fcfe
philhubb <feeliponyenobi@gmail.com>
2022-05-25 03:33:05 +0100

    Tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 /**
  * add_node_end - Adds a new node at the end of a list.
  * @head: Address of the first node of a list.
@@ -34,4 +35,4 @@ list_t *add_node_end(list_t **head, const char *str)
 		temp->next = new_node;
 	}
 	return (new_node);
-}
+
