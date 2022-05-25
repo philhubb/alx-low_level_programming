eab7e5ee06f6df0e6b294fd52a800482e58f716e
philhubb <feeliponyenobi@gmail.com>
2022-05-25 03:50:24 +0100

    Tasks

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+	  global    main
+          extern    printf
+main:
+	  push	rbp
+	  mov	rdi,fmt
+	  mov	rsi,msg
+	  mov	rax,0
+	  call	printf
+	  pop	rbp
+	  mov	rax,0
+	  ret
+format: db `Hello, Holberton\n`,0
