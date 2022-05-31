3695643063ba071eadce33b9e9b1908b7619b7b7
philhubb <feeliponyenobi@gmail.com>
2022-05-31 04:00:48 +0100

    Mandatory task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * binary_to_uint - A function that converts a binary number to an unsigned int
+  * @b: binary number
+  * Return: the converted number or 0 if it fails
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int base = 1, result = 0, len = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[len])
+		len++;
+
+	while (len)
+	{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+			return (0);
+
+		if (b[len - 1] == '1')
+			result += base;
+		base *= 2;
+		len--;
+	}
+	return (result);
+}
